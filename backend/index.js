2979b76610a3d76c6e9a488a0b359f1a405bf9ae
Rahmat ullah <baheer224@gmail.com>
2023-03-09 11:52:30 +0500

    march_9th

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 require('dotenv').config()
-
-
-
-
 const PORT = process.env.PORT || 8000
 app.listen(PORT , ()=>{
      console.log(`Your Server is On PORT : ${PORT}`);
